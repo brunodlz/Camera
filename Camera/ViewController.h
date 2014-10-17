57db910a33a744fa1b65eedcacfb7e717f7ccccc
Bruno da Luz <brunolabx@gmail.com>
2014-10-17 11:51:43 -0300

    Camera
    App utilizando a câmera do dispositivo.

--- Camera/ViewController.h
+++ Camera/ViewController.h
@@ -8,7 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (strong, nonatomic) UIImagePickerController *pickerCamera;
+@property (strong, nonatomic) UIImagePickerController *pickerLibrary;
+@property (strong, nonatomic) UIImage *image;
+
+- (IBAction)actionCamera:(id)sender;
+- (IBAction)actionLibrary:(id)sender;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 
 @end
--- Camera/ViewController.m
+++ Camera/ViewController.m
@@ -24,4 +24,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)actionCamera:(id)sender {
+    self.pickerCamera = [[UIImagePickerController alloc] init];
+    self.pickerCamera.delegate = self;
+    [self.pickerCamera setSourceType:UIImagePickerControllerSourceTypeCamera];
+    [self presentViewController:self.pickerCamera animated:YES completion:nil];
+}
+
+- (IBAction)actionLibrary:(id)sender {
+    self.pickerLibrary = [[UIImagePickerController alloc] init];
+    self.pickerLibrary.delegate = self;
+    [self.pickerLibrary setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    [self presentViewController:self.pickerLibrary animated:YES completion:nil];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    self.image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    [self.imageView setImage:self.image];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 @end
