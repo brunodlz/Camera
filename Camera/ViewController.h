89841e09ca323352d1221f782f1a11cf2050d254
Bruno da Luz <brunolabx@gmail.com>
2014-10-17 11:38:32 -0300

    Initial Commit

---
+++ Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by bruno da luz on 10/17/14.
+//  Copyright (c) 2014 bruno da luz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
